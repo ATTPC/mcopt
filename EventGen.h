395a3f11bf66e59142f99e8dcd9e07a010829fb3
Joshua Bradt <bradt@nscl.msu.edu>
2016-01-08 15:05:22 -0500

    Fixed other codes to account for refactoring.

--- EventGen.h
+++ EventGen.h
@@ -11,7 +11,7 @@ namespace mcopt
     arma::mat calibrate(const Track& tr, const arma::vec& vd, const double clock);
     arma::mat uncalibrate(const Track& tr, const arma::vec& vd, const double clock, const int offset=0);
 
-    std::map<uint16_t, unsigned long> findHitPads(const PadPlane& pads, const Track& tr, const arma::vec& vd,
+    std::map<uint16_t, unsigned long> makePeaksFromSimulation(const PadPlane& pads, const Track& tr, const arma::vec& vd,
                                                   const double clock, const int massNum, const double ioniz);
 }
 
